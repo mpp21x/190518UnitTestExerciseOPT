c9297102a6c38028a231e7ff69c0f8e3ef79ce5e
matt <matt@zuso.ai>
2020-03-15 19:01:23 +0800

    完成第三題

--- tests/AuthenticationServiceTest.php
+++ tests/AuthenticationServiceTest.php
@@ -14,24 +14,42 @@ use PHPUnit\Framework\TestCase;
 class AuthenticationServiceTest extends TestCase
 {
     use MockeryPHPUnitIntegration;
+    /** @var ProfileDaoInterface|MockInterface */
+    private $stubProfileDao;
+    /** @var RsaTokenDaoInterface|MockInterface */
+    private $stubRsaTokenDao;
+
     /** @test */
     public function is_valid_test()
     {
-        /** @var ProfileDaoInterface|MockInterface $profileDao */
-        $profileDao = Mockery::mock(ProfileDaoInterface::class);
-        $profileDao->shouldReceive("getPassword")
-            ->with("joey")
-            ->andReturn("91");
+        $account = "joey";
+        $this->givenProfile($account, "91");
 
-        /** @var RsaTokenDaoInterface|MockInterface $rsaTokenDao */
-        $rsaTokenDao = Mockery::mock(RsaTokenDaoInterface::class);
-        $rsaTokenDao->shouldReceive("getRandom")
-            ->with("joey")
-            ->andReturn("000000");
+        $this->givenToken($account, "000000");
 
-        $target = new AuthenticationService($profileDao, $rsaTokenDao);
-        $actual = $target->isValid("joey", "91000000");
+        $target = new AuthenticationService($this->stubProfileDao, $this->stubRsaTokenDao);
+        $actual = $target->isValid($account, "91000000");
         $this->assertTrue($actual);
+    }
+
+    protected function givenProfile(string $account, string $password): void
+    {
+        $this->stubProfileDao->shouldReceive("getPassword")
+            ->with($account)
+            ->andReturn($password);
+    }
+
+    protected function givenToken(string $account, string $token): void
+    {
+        $this->stubRsaTokenDao->shouldReceive("getRandom")
+            ->with($account)
+            ->andReturn($token);
+    }
+
+    protected function setUp()
+    {
+        $this->stubProfileDao = Mockery::mock(ProfileDaoInterface::class);
+        $this->stubRsaTokenDao = Mockery::mock(RsaTokenDaoInterface::class);
     }
 
 }
