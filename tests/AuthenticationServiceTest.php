9e49baae80b7c73c7faa2ee02ed6d8be67f52d88
matt <matt@zuso.ai>
2020-03-15 19:15:42 +0800

    完成第五題

--- tests/AuthenticationServiceTest.php
+++ tests/AuthenticationServiceTest.php
@@ -11,49 +11,74 @@ use Mockery;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
 use Mockery\MockInterface;
 use PHPUnit\Framework\TestCase;
+use function strpos;
 
 class AuthenticationServiceTest extends TestCase
 {
     use MockeryPHPUnitIntegration;
     /** @var ProfileDaoInterface|MockInterface */
-    private $stubProfileDao;
+    private $stubProfile;
     /** @var RsaTokenDaoInterface|MockInterface */
-    private $stubRsaTokenDao;
+    private $stubRsa;
     /** @var LoggerInterface|MockInterface */
     private $logger;
+    /** @var AuthenticationService */
+    private $target;
+
+    protected function setUp()
+    {
+        $this->stubProfile = Mockery::mock(ProfileDaoInterface::class);
+        $this->stubRsa = Mockery::mock(RsaTokenDaoInterface::class);
+        $this->logger = Mockery::mock(LoggerInterface::class);
+        $this->target = new AuthenticationService($this->stubProfile, $this->stubRsa, $this->logger);
+    }
 
     /** @test */
     public function is_valid_test()
     {
         $account = "joey";
-        $this->givenProfile($account, "91");
+        $password = "91";
+        $this->givenProfile($account, $password);
 
-        $this->givenToken($account, "000000");
+        $token = "000000";
+        $this->givenToken($account, $token);
 
-        $target = new AuthenticationService($this->stubProfileDao, $this->stubRsaTokenDao, $this->logger);
-        $actual = $target->isValid($account, "91000000");
-        $this->assertTrue($actual);
+        $this->shouldBeValid($account, $password . $token);
+    }
+
+    /** @test */
+    public function log_account_when_invalid()
+    {
+        $this->givenProfile('joey', '91');
+        $this->givenToken('joey', '000000');
+
+        $this->logger->shouldReceive('save')
+            ->with(Mockery::on(function ($message) {
+                return strpos($message, 'joey') !== false;
+            }))->once();
+
+        $this->target->isValid('joey', 'wrong password');
     }
 
     protected function givenProfile(string $account, string $password): void
     {
-        $this->stubProfileDao->shouldReceive("getPassword")
+        $this->stubProfile->shouldReceive("getPassword")
             ->with($account)
             ->andReturn($password);
     }
 
     protected function givenToken(string $account, string $token): void
     {
-        $this->stubRsaTokenDao->shouldReceive("getRandom")
+        $this->stubRsa->shouldReceive("getRandom")
             ->with($account)
             ->andReturn($token);
     }
 
-    protected function setUp()
+    protected function shouldBeValid(string $account, string $passwordWithToken): void
     {
-        $this->stubProfileDao = Mockery::mock(ProfileDaoInterface::class);
-        $this->stubRsaTokenDao = Mockery::mock(RsaTokenDaoInterface::class);
-        $this->logger = Mockery::mock(LoggerInterface::class);
+        $actual = $this->target->isValid($account, $passwordWithToken);
+        $this->assertTrue($actual);
     }
+
 
 }
